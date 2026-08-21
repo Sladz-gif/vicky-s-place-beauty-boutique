2be181ff5f4742ed7a2287026e9317c3aadd1d65
Sladz-gif <Sladz-gif@users.noreply.github.com>
2026-08-21 12:42:21 +0000

    chore: remove old SSR build artifacts from git tracking

--- .vercel/output/static/assets/smartphone-BlvB1BDZ.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-CaYjXmwE.js";var t=e(`credit-card`,[[`rect`,{width:`20`,height:`14`,x:`2`,y:`5`,rx:`2`,key:`ynyp8z`}],[`line`,{x1:`2`,x2:`22`,y1:`10`,y2:`10`,key:`1b3vmo`}]]),n=e(`smartphone`,[[`rect`,{width:`14`,height:`20`,x:`5`,y:`2`,rx:`2`,ry:`2`,key:`1yt0o3`}],[`path`,{d:`M12 18h.01`,key:`mhygvu`}]]);export{t as n,n as t};
